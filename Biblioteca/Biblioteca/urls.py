48f733760995f8daef7a8ad165395b03cdc5085b
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-23 09:49:57 -0600

    Creada e instalada la aplicación de catálogo

--- Biblioteca/Biblioteca/urls.py
+++ Biblioteca/Biblioteca/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='catalogo/', permanent=True)),
+    path('catalogo/', include('catalogo.urls')),
 ]
