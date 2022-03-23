7e8b3f83de813713618b50e72bb0481741e8cd58
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-23 10:17:38 -0600

    Arreglado el URL para archivos estáticos

--- Biblioteca/Biblioteca/urls.py
+++ Biblioteca/Biblioteca/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='catalogo/', permanent=True)),
     path('catalogo/', include('catalogo.urls')),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
