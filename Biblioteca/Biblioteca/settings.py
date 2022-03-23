41774a46d900add86437c17e1f899fe873f5c3dc
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-23 10:27:02 -0600

    Agregadas utilidades de seed y soft delete

--- Biblioteca/Biblioteca/settings.py
+++ Biblioteca/Biblioteca/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalogo.apps.CatalogoConfig',
+    'safedelete',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
