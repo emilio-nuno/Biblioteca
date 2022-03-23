7f182cfbc89a3f56f9dcb848b0aaf5b62dc356ed
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-23 09:50:33 -0600

    Agregado URL estático y urls del catalogo

--- Biblioteca/Biblioteca/urls.py
+++ Biblioteca/Biblioteca/urls.py
@@ -17,9 +17,12 @@ from django.urls import include
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='catalogo/', permanent=True)),
     path('catalogo/', include('catalogo.urls')),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
